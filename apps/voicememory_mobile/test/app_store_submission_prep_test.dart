1842306ed01ccad7fa05317e16c19c7123d34158
Chirag Patel <invoicessutton@gmail.com>
2026-06-28 19:48:59 +0100

    Bump TestFlight build number to 45

--- apps/voicememory_mobile/test/app_store_submission_prep_test.dart
+++ apps/voicememory_mobile/test/app_store_submission_prep_test.dart
@@ -128,7 +128,7 @@ void main() {
       _expectNoBannedCopy([AppStoreSubmissionCopy.privacyExplanationBody]);
     });
 
-    test('build number remains 0.2.0+43', () {
+    test('build number remains 0.2.0+45', () {
       final pubspec = File('pubspec.yaml').readAsStringSync();
       expect(pubspec, contains('version: ${AppStoreSubmissionCopy.expectedVersionLine}'));
       expect(AppStoreSubmissionCopy.expectedVersion, '0.2.0');
